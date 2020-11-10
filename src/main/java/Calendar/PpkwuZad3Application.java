85cf3d2111bbfbc0453a1e2278195831656b5305
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-10 14:55:13 +0100

    Initial commit, generated skeleton form spring initializer

---
+++ src/main/java/Calendar/PpkwuZad3Application.java
@@ -0,0 +1,13 @@
+package Calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZad3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuZad3Application.class, args);
+	}
+
+}
