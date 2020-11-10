280d917513fb90439ba016f22f9a2a7dc0ecbdc4
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-10 15:28:12 +0100

    function getCalendarForRequestedMonthFromWEEIA now collects html code for requested month from weeia site

--- src/main/java/Calendar/DataCollector.java
+++ src/main/java/Calendar/DataCollector.java
@@ -4,7 +4,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @RestController public class DataCollector {
 
+    @RequestMapping(path = "/analyseString")
+    public String getCalendarForRequestedMonthFromWEEIA(@RequestParam(value = "month", required = true) int month,
+            @RequestParam(value = "year", required = true) int year) {
 
+        String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?";
+        String yearBuilder = "rok=";
+        String monthBuilder = "&miesiac=";
+
+        StringBuilder result = new StringBuilder();
+
+        URL oracle = null;
+        try {
+            url += yearBuilder + year + monthBuilder;
+            if (month < 10) {
+                url += "0";
+            }
+            oracle = new URL(url + month);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                result.append(inputLine);
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return result.toString();
+    }
 }
