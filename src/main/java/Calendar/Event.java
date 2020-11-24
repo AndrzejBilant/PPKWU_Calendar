0796c56c9ba737206cecaa44ccb05ea941cce476
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-24 14:30:04 +0100

    added container class for events

---
+++ src/main/java/Calendar/Event.java
@@ -0,0 +1,18 @@
+package Calendar;
+
+class Event
+{
+    String link;
+    String date;
+    String text;
+    String year;
+    String month;
+
+    public Event(String link, String date, String text, String year, String month) {
+        this.link = link;
+        this.date = date;
+        this.text = text;
+        this.year = year;
+        this.month = month;
+    }
+}
