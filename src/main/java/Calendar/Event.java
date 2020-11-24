ae0e84b48f1785dfdec9700e253ba7f52bcdcf1a
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-24 15:01:50 +0100

    Event class refactor

--- src/main/java/Calendar/Event.java
+++ src/main/java/Calendar/Event.java
@@ -1,7 +1,7 @@
 package Calendar;
 
-class Event
-{
+class Event {
+
     String link;
     String date;
     String text;
@@ -17,19 +17,29 @@ class Event
         this.year = year;
         this.month = month;
 
-        this.dtstamp = year+month+date+"T000000Z";
+        this.dtstamp = year + month + date + "T000000Z";
         this.uid = "-1234@example.com";
     }
 
-    public String generateICal(){
-        String result = "BEGIN:VEVENT\n" + "UID:"+dtstamp+uid+"\r\n"
-                        + "DTSTAMP:" + dtstamp+"\r\n"
-                        + "DTSTART:"+dtstamp+ "\r\n"
-                        + "SUMMARY:"+text+"\r\n";
-        if(link!=null){
-            result=result+"URL:"+link+"\r\n";
+    public String generateICal() {
+        String result = "BEGIN:VEVENT\n"
+                        + "UID:"
+                        + dtstamp
+                        + uid
+                        + "\r\n"
+                        + "DTSTAMP:"
+                        + dtstamp
+                        + "\r\n"
+                        + "DTSTART:"
+                        + dtstamp
+                        + "\r\n"
+                        + "SUMMARY:"
+                        + text
+                        + "\r\n";
+        if (link != null) {
+            result = result + "URL:" + link + "\r\n";
         }
-        result=result+ "END:VEVENT\r\n";
+        result = result + "END:VEVENT\r\n";
 
         return result;
     }
