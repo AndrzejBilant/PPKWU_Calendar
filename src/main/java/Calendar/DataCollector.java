3804cded06023460b0604a226fe371ab32b1431e
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-24 14:18:02 +0100

    refactored

--- src/main/java/Calendar/DataCollector.java
+++ src/main/java/Calendar/DataCollector.java
@@ -8,17 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @RestController public class DataCollector {
 
-    @RequestMapping(path = "/analyseString")
-    public String getCalendarForRequestedMonthFromWEEIA(@RequestParam(value = "month", required = true) int month,
-            @RequestParam(value = "year", required = true) int year) {
+    @RequestMapping(path = "/calendar")
+    public String getCalendarForRequestedMonthFromWEEIA(@RequestParam(value = "month") int month,
+            @RequestParam(value = "year") int year) {
 
         String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?";
         String yearBuilder = "rok=";
