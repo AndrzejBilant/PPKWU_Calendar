ecc9918b0b22180839a218e48a6a469f8e6376c8
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-11-24 15:08:21 +0100

    Controller class refactor

--- src/main/java/Calendar/DataCollector.java
+++ src/main/java/Calendar/DataCollector.java
@@ -16,16 +16,69 @@ import java.util.ArrayList;
 @RestController public class DataCollector {
 
     @RequestMapping(path = "/calendar")
-    public String getCalendarForRequestedMonthFromWEEIA(@RequestParam(value = "month") int month,
-            @RequestParam(value = "year") int year, HttpServletResponse response) {
+    public void getCalendarForRequestedMonthFromWEEIA(@RequestParam(value = "month") int month, @RequestParam(value = "year") int year,
+            HttpServletResponse response) {
 
+        Document document = getDataFromWebsite(month, year);
+        StringBuilder result = new StringBuilder(
+                "BEGIN:VCALENDAR\r\n" + "VERSION:2.0\r\n" + "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n");
+        ArrayList<Event> events = getEvents(month, year, document);
+        for (Event e : events) {
+            String temp = e.generateICal();
+            result.append(temp);
+        }
+        result.append("END:VCALENDAR");
+        File file = generateFile(result.toString(), month, year);
+
+        sendFile(response, file);
+    }
+
+    private void sendFile(HttpServletResponse response, File file) {
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            response.setContentType("text/calendar;charset=utf-8");
+            IOUtils.copy(inputStream, response.getOutputStream());
+            response.flushBuffer();
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private ArrayList<Event> getEvents(int month, int year, Document document) {
+        ArrayList<Event> events = new ArrayList();
+        Elements elements = document.select("td");
+        for (Element element : elements) {
+            if (element.attr("class").equals("active")) {
+                Elements date = element.select("a[href]");
+                String link = date.attr("href");
+                if (link.contains("javascript:void()")) {
+                    link = null;
+                }
+                Elements text = element.getElementsByTag("p");
+
+                String tempMonth = "";
+                if (month < 10) {
+                    tempMonth = tempMonth + "0";
+                }
+                tempMonth += String.valueOf(month);
+                String tempDay = "";
+                if (Integer.valueOf(date.text()) < 10) {
+                    tempDay += "0";
+                }
+                tempDay += date.text();
+                events.add(new Event(link, tempDay, text.text(), String.valueOf(year), tempMonth));
+
+            }
+        }
+        return events;
+    }
+
+    private Document getDataFromWebsite(int month, int year) {
         String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?";
         String yearBuilder = "rok=";
         String monthBuilder = "&miesiac=";
-
-        StringBuilder result = new StringBuilder("BEGIN:VCALENDAR\r\n"
-                                                 + "VERSION:2.0\r\n"
-                                                 + "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n");
 
         url += yearBuilder + year + monthBuilder;
         if (month < 10) {
@@ -38,57 +91,11 @@ import java.util.ArrayList;
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
-
-        ArrayList<Event> events = new ArrayList();
-        Elements elements = document.select("td");
-
-        for (Element element : elements) {
-            if (element.attr("class").equals("active")) {
-                Elements date = element.select("a[href]");
-                String link = date.attr("href");
-                if(link.contains("javascript:void()")){
-                    link=null;
-                }
-                Elements text = element.getElementsByTag("p");
-
-                String tempMonth ="";
-                if(month < 10){
-                    tempMonth = tempMonth+"0";
-                }
-                tempMonth+=String.valueOf(month);
-                String tempDay = "";
-                if(Integer.valueOf(date.text())<10)
-                {
-                    tempDay+="0";
-                }
-                tempDay+=date.text();
-                events.add(new Event(link,tempDay,text.text(),String.valueOf(year), tempMonth));
-
-            }
-        }
-        for(Event e: events)
-        {
-            String elo = e.generateICal();
-            result.append(elo);
-        }
-        result.append("END:VCALENDAR");
-        File file = generateFile(result.toString(),month,year);
-
-        try {
-            InputStream inputStream = new FileInputStream(file);
-            response.setContentType("text/calendar;charset=utf-8");
-            IOUtils.copy(inputStream, response.getOutputStream());
-            response.flushBuffer();
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-
-        return result.toString();
+        return document;
     }
-    public File generateFile(String content, int month,int year){
-        String name = month+year+".ics";
+
+    public File generateFile(String content, int month, int year) {
+        String name = month + year + ".ics";
         File file = new File(name);
         try {
             FileWriter fileWriter = new FileWriter(name);
